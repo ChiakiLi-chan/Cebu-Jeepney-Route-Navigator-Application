6892485404604f86b7615ff64f2823090a34fbd6
Li~chan <137862600+ChiakiLi-chan@users.noreply.github.com>
2026-04-17 16:41:31 +0800

    Initial commit

---
+++ lib/models/gps_point.dart
@@ -0,0 +1,11 @@
+class GPSPoint {
+  final double latitude;
+  final double longitude;
+  final DateTime timestamp;
+
+  GPSPoint({
+    required this.latitude,
+    required this.longitude,
+    required this.timestamp,
+  });
+}
---
+++ lib/models/route_model.dart
@@ -0,0 +1,9 @@
+class JeepneyRoute {
+  final String name;
+  final List<String> stops;
+
+  JeepneyRoute({
+    required this.name,
+    required this.stops,
+  });
+}
